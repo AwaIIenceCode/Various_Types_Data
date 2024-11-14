f5ed395ae588a8cc29ceb251433d3360df5b24f5
Oleh Skrypnyk-Zakrevskiy <awallencecode@gmail.com>
2024-11-14 16:25:42 +0200

    ex_1 is done

---
+++ Various_Types/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	int year;
+
+	cout << "Enter a year: ";
+	cin >> year;
+
+	int days = 365 + (year % 4 == 0 && year % 100 != 0 || year % 400 == 0);
+
+	cout << "The number of days in the year " << year << " is " << days << endl;
+
+	return 0;
+}
